d2caf72a40d5301b9ea7e143a10d7d8bf4f39b85
Jishii <33364545+smudge121@users.noreply.github.com>
2022-11-18 19:09:47 -0800

    added a save system that saves each entry on button press and loads on page load

--- Develop/script.js
+++ Develop/script.js
@@ -1,38 +1,32 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-var saveButtons = $("[aria-label='save']");
+//
 
-var saveText = [];
+
+$(function () {
+  // adds a listener to each button.  The listener will save all the entries simultaneously.
+var saveButtons = $("[aria-label='save']");
+var saveText = JSON.parse(localStorage.getItem("saveText")) || [];
+
 
 saveButtons.each(function(index){
-  // console.log($(this).text());
+
+
   $(this).on('click', function(event){
-    // console.log($(event.target).parent().attr('id'));
-    var day = {
-      time: $(event.target).parent().attr('id'),
-      text: $(event.target).siblings('textarea').text()
-    }
-    saveText.push(day);
+
+    saveButtons.each(function(ind){
+      var id = $(saveButtons[ind]).parent().attr('id');
+      var time = id.replace("hour-","");
+      saveText[parseInt(time,10) - 9] = $(saveButtons[ind]).siblings('textarea').val();
+    });
+
     localStorage.setItem("saveText",JSON.stringify(saveText));
+
   })
 });
 
 
 
   
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  //check hour block to current hour to determine css styling, if its past present or future
   var hourBlocks = $('.time-block');
   for (var i = 0; i < hourBlocks.length; i++){
     var hour = $(hourBlocks[i]).attr('id').replace("hour-","");
@@ -51,16 +45,20 @@ saveButtons.each(function(index){
 
 
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+// Sets the values of the text areas of a local save file exists
   
+  if (saveText.length != 0)
+  {
+    saveButtons.each(function(index){
+      $(saveButtons[index]).siblings('textarea').text(saveText[index]);
+  
+    });
+  }
 
 
-
-  // TODO: Add code to display the current date in the header of the page.
+  // Display current day
   var currentDay = $('#currentDay');
-  currentDay.text(dayjs().format('dddd, MMMM d, YYYY'));
+  currentDay.text(dayjs().format('dddd, MMMM D, YYYY'));
 
 
 
